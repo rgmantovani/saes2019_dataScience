d441c2988c8798da704080306622621b08d06eda
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2019-08-29 16:17:41 -0300

    updating script

--- scripts/downloadDatasets.R
+++ scripts/downloadDatasets.R
@@ -6,7 +6,7 @@ data.ids = c(61, 1557, 1499, 54, 1131)
 cat("<<<<  Saving all datasets  >>>>\n")
 for (id in data.ids) {
     data = getOMLDataSet(data.id = id)
-    dataName = paste0("../../datasets/dataid_", id, "_", data$desc$name, ".arff")
+    dataName = paste0("../datasets/dataid_", id, "_", data$desc$name, ".arff")
     writeARFF(x = data$data, path = dataName, overwrite = TRUE)
 }
 cat("-----------------------------------------------------------\n\n")
@@ -14,7 +14,7 @@ cat("-----------------------------------------------------------\n\n")
 cat("<<<<  Reading all datasets  >>>>\n")
 # read all datasets
 savedData = NULL
-datasets  = list.files(path = "../../datasets", full.names = TRUE)
+datasets  = list.files(path = "../datasets", full.names = TRUE)
 for (dataset in datasets) {
     savedData = readARFF(dataset)
     print(dim(savedData))
